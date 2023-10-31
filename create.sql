16f9a8685a1ccaedd755c0095c81d8f8d8bcf080
ElixxRadeS <omerfarukaras16@gmail.com>
2023-10-31 14:37:20 +0300

    walla ben yazmadım

---
+++ create.sql
@@ -0,0 +1,6 @@
+INSERT INTO musteriler (ad, soyad, adres, isyeri, unvan, tel,tc)
+VALUES ('Ömer FARUK', 'ARAS', 'Skagen 21', 'Stavanger', 'OGRENCI', 5304653663,15567970942);
+
+
+INSERT INTO araclar (SASI_NO, model, yil, renk, fiyat, arac_durumu,MUSTERI_ID)
+VALUES (11448146, "RS6",2000,"siyah",44444444,"satılık",1)
